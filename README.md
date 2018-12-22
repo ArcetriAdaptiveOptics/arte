89f56b4089d83721574c6e921fc9348568148746
lbusoni <lbusoni@gmail.com>
2018-12-22 20:52:44 +0100

    add travis

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # apposto - Arcetri Python adaPtive OpticS TOols
 A collection of tools for adaptive optics. Too much generic? Yes, I know
+
+
+[![Build Status][travis]][travislink]  [![Coverage Status][coveralls]][coverallslink]  [![PyPI version][pypiversion]][pypiversionlink]
+
+
+[travis]: https://travis-ci.com/lbusoni/apposto.svg?branch=master "go to travis"
+[travislink]: https://travis-ci.com/lbusoni/apposto
+[coveralls]: https://coveralls.io/repos/github/lbusoni/apposto/badge.svg?branch=master "go to coveralls"
+[coverallslink]: https://coveralls.io/github/lbusoni/apposto
+[pypiversion]: https://badge.fury.io/py/apposto.svg
+[pypiversionlink]: https://badge.fury.io/py/apposto
--- setup.py
+++ setup.py
@@ -1,26 +1,91 @@
 #!/usr/bin/env python
-from setuptools import setup
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import setup, Command
+
+NAME = 'apposto'
+DESCRIPTION = 'Arcetri Python adaPtive OpticS TOols'
+URL = 'https://github.com/lbusoni/apposto'
+EMAIL = 'lorenzo.busoni@inaf.it'
+AUTHOR = 'Lorenzo Busoni'
+LICENSE= 'MIT'
+KEYWORDS= 'Adaptive Optics, Astrophysics, INAF, Arcetri',
 
 
-__version__ = "$Id: setup.py 199 2017-01-14 21:27:28Z lbusoni $"
+here = os.path.abspath(os.path.dirname(__file__))
+# Load the package's __version__.py module as a dictionary.
+about = {}
+with open(os.path.join(here, NAME, '__version__.py')) as f:
+    exec(f.read(), about)
 
-setup(name='apposto',
-      description='Arcetri Python adaPtive OpticS TOols',
-      version='0.1',
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
+setup(name=NAME,
+      description=DESCRIPTION,
+      version=about['__version__'],
       classifiers=['Development Status :: 4 - Beta',
                    'Operating System :: POSIX :: Linux',
-                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6',
                    ],
       long_description=open('README.md').read(),
-      url='',
-      author='lorenzo.busoni@inaf.it',
-      license='MIT',
-      keywords='adaptive optics',
+      url=URL,
+      author_email=EMAIL,
+      author=AUTHOR,
+      license=LICENSE,
+      keywords=KEYWORDS,
       packages=['apposto',
+                'apposto.atmo',
+                'apposto.misc',
+                'apposto.types',
+                'apposto.utils',
                 ],
       install_requires=["numpy",
                         "scipy",
                         "matplotlib",
+                        "astropy"
                         ],
       include_package_data=True,
+      test_suite='test',
+      cmdclass={'upload': UploadCommand, },
       )
