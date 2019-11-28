deb6a230597d9b1ce9b929e12a27ebefa1ee2c4e
g-carla <giulia.carl9@gmail.com>
2019-11-28 10:37:35 +0100

    Comments corrected.

--- apposto/types/aperture.py
+++ apposto/types/aperture.py
@@ -15,12 +15,9 @@ class CircularOpticalAperture():
     aperture_radius: float
         Radius of the optical aperture in [meters].
 
-    polar_coords: tuple of two floats
-        Polar coordinates (rho, theta) of the aperture center
-        in [arcsec, degrees].
-
-    height: float
-        Height of the aperture center in [meters].
+    cartes_coords: tuple of three floats
+        Cartesian coordinates (x, y, z) of the aperture center
+        in [m].
     '''
 
     def __init__(self, aperture_radius, cartes_coords):
@@ -34,27 +31,3 @@ class CircularOpticalAperture():
 
     def getApertureRadius(self):
         return self._r
-
-#     def __init__(self, aperture_radius, polar_coords, height):
-#         self._r = aperture_radius
-#         self._rho = polar_coords[0]
-#         self._theta = polar_coords[1]
-#         self._z = height
-#
-#     @staticmethod
-#     def fromDegToRad(angle_deg):
-#         return angle_deg * np.pi / 180
-#
-#     @staticmethod
-#     def fromPolarToCartesian(rho, thetaRad):
-#         x = rho * np.cos(thetaRad)
-#         y = rho * np.sin(thetaRad)
-#         return x, y
-#
-#     def getApertureCenterCartesianCoords(self):
-#         x, y = self.fromPolarToCartesian(self._rho,
-#                                          self.fromDegToRad(self._theta))
-#         return np.array([x, y, self._z])
-#
-#     def getApertureCenterPolarCoords(self):
-#         return np.array([self._rho, self._theta, self._z])
