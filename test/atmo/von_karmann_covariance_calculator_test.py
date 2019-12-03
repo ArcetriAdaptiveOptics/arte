9db7f3681048f94856fade2daa129dd348f2e2c8
g-carla <giulia.carl9@gmail.com>
2019-12-03 17:44:00 +0100

    Added test on s_l.

--- test/atmo/von_karmann_covariance_calculator_test.py
+++ test/atmo/von_karmann_covariance_calculator_test.py
@@ -3,7 +3,7 @@ import unittest
 import numpy as np
 from apposto.atmo.von_karmann_covariance_calculator import \
     VonKarmannSpatioTemporalCovariance
-from apposto.atmo.cn2_profile import Cn2Profile
+from apposto.atmo.cn2_profile import Cn2Profile, EsoEltProfiles
 from apposto.types.guide_source import GuideSource
 from apposto.types.aperture import CircularOpticalAperture
 from test.test_helper import setUpLogger
@@ -34,6 +34,19 @@ class VonKarmanCovarianceCalculatorTest(unittest.TestCase):
             cn2, source, source, aperture, aperture, spatial_freqs)
 
         self.assertEqual(0, vk_cov.getZernikeCovariance(2, 3))
+
+    def testLayerProjectedSeparationOnXYPlane(self):
+        cn2 = EsoEltProfiles.Median()
+        source1 = GuideSource((0, 0), np.inf)
+        source2 = GuideSource((50, 30), 100e3)
+        aperture = CircularOpticalAperture(10, [0, 0, 0])
+        spatial_freqs = np.logspace(-2, 2, 100)
+        vk_cov = VonKarmannSpatioTemporalCovariance(
+            cn2, source1, source2, aperture, aperture, spatial_freqs)
+        s_l = np.array([
+            vk_cov.layerProjectedAperturesSeparation(n)[2]
+            for n in range(vk_cov._layersAlt.shape[0])])
+        self.assertEqual(np.zeros(vk_cov._layersAlt.shape[0]).all(), s_l.all())
 
 
 if __name__ == "__main__":
