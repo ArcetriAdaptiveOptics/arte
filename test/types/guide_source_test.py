f8697c4a787aaf8408dc3c4667f6f81097d7736d
g-carla <giulia.carl9@gmail.com>
2019-12-13 11:24:46 +0100

    test: written a small test for guide_source to check the use of astropy.units.

---
+++ test/types/guide_source_test.py
@@ -0,0 +1,35 @@
+'''
+Created on 13 dic 2019
+
+@author: giuliacarla
+'''
+
+import unittest
+import astropy.units as u
+from astropy.tests.helper import assert_quantity_allclose
+from apposto.types.guide_source import GuideSource
+
+
+class GuideSourceTest(unittest.TestCase):
+
+    def testOutputAsInputPlusUnits(self):
+        rho, theta = (50, 90)
+        z = 100e3
+        source = GuideSource((rho, theta), z)
+
+        want = [50 * u.arcsec, 90 * u.deg, 100e3 * u.m]
+        got = source.getSourcePolarCoords()
+        assert_quantity_allclose(got[0], want[0])
+        assert_quantity_allclose(got[1], want[1])
+        assert_quantity_allclose(got[2], want[2])
+
+    def testCartesianCoords(self):
+        rho, theta = (50, 90)
+        z = 100e3
+        source = GuideSource((rho, theta), z)
+
+        want = [0 * u.arcsec, 50 * u.arcsec, 100e3 * u.m]
+        got = source.getSourceCartesianCoords()
+        assert_quantity_allclose(got[0], want[0], atol=1e-14 * u.arcsec)
+        assert_quantity_allclose(got[1], want[1])
+        assert_quantity_allclose(got[2], want[2])
