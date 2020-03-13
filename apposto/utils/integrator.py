508fdec24912e61732028d69665ae90e9a7099f1
g-carla <giulia.carl9@gmail.com>
2020-03-13 17:14:44 +0100

    utils: initialized 'integrator.py'

--- apposto/utils/integrator.py
+++ apposto/utils/integrator.py
@@ -7,18 +7,38 @@ Created on 13 mar 2020
 
 class Integrator():
 
-    def __init__(self, delay, gain):
-        self.delay = delay
-        self.gain = gain
+    def __init__(self):
+        self.delay = None
+        self.gain = None
+        self.rtf = None
+        self.ntf = None
+
+    def setDelay(self, d):
+        self.delay = d
+
+    def setGain(self, g):
+        self.gain = g
+
+    def setRejectionTransferFunction(self, rtf):
+        self._rtf = rtf
+
+    def setNoiseTransferFunction(self, ntf):
+        self._ntf = ntf
+
+    def getRejectionTransferFunction(self, temporal_freqs=None):
+        if self._rtf is None:
+            self._computeRTF()
+        return self._rtf
+
+    def getNoiseTransferFunction(self, temporal_freqs=None):
+        if self._ntf is None:
+            self._computeNTF()
+        return self._ntf
 
     def _computeRTF(self):
+        self._rtf
         pass
 
     def _computeNTF(self):
-        pass
-
-    def getRejectionTransferFunction(self):
-        pass
-
-    def getNoiseTransferFunction(self):
+        self._ntf
         pass
