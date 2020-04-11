e8f14db73d11ce0df7b2842e8ba52b27ba336b42
Alfio Puglisi <alfio.puglisi@inaf.it>
2020-04-11 15:22:02 +0200

    Added capture_output

---
+++ arte/utils/capture_output.py
@@ -0,0 +1,24 @@
+
+from io import StringIO
+from contextlib import contextmanager
+
+@contextmanager
+def capture_output():
+    '''
+    Use this context manager to capture stdout and stderr
+    from a Python code section. Example:
+
+    with capture_output() as (out, err):
+        routines_that_prints_lots()
+
+    out.getvalue() will be the stdout
+    err.getvalue() will be the stderr
+    '''
+    new_out, new_err = StringIO(), StringIO()
+    old_out, old_err = sys.stdout, sys.stderr
+    try:
+        sys.stdout, sys.stderr = new_out, new_err
+        yield sys.stdout, sys.stderr
+    finally:
+        sys.stdout, sys.stderr = old_out, old_err
+
