e0ed051db4c562d873e35c9492b0662a445525d2
Alfio Puglisi <alfio.puglisi@inaf.it>
2020-04-11 15:27:47 +0200

    Added routines from FLAO PyModules

---
+++ arte/contrib/InterpolatedArray.py
@@ -0,0 +1,37 @@
+#
+# (c) Mark Ivey 2008
+#
+# http://zovirl.com/2008/11/04/interpolated-lookup-tables-in-python/
+
+class InterpolatedArray(object):
+
+  """An array-like object that provides
+  interpolated values between set points."""
+
+  def __init__(self, points):
+    self.points = sorted(points)
+
+  def __getitem__(self, x):
+    if x < self.points[0][0] or x > self.points[-1][0]:
+      raise ValueError
+    lower_point, upper_point = self._GetBoundingPoints(x)
+    return self._Interpolate(x, lower_point, upper_point)
+
+  def _GetBoundingPoints(self, x):
+    """Get the lower/upper points that bound x."""
+    lower_point = None
+    upper_point = self.points[0]
+    for point  in self.points[1:]:
+      lower_point = upper_point
+      upper_point = point
+      if x <= upper_point[0]:
+        break
+    return lower_point, upper_point
+
+  def _Interpolate(self, x, lower_point, upper_point):
+    """Interpolate a Y value for x given lower & upper
+    bounding points."""
+    slope = (float(upper_point[1] - lower_point[1]) /
+             (upper_point[0] - lower_point[0]))
+    return lower_point[1] + (slope * (x - lower_point[0]))
+
