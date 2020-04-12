e4ad1c2e182ab57ab1fb9220739f22c33032597e
Alfio Puglisi <alfio.puglisi@inaf.it>
2020-04-12 20:43:37 +0200

    Added if_ decorator

---
+++ arte/contrib/if_.py
@@ -0,0 +1,25 @@
+
+def if_(condition, warning=None):
+    '''
+    Decorator to turn a function into a NO-OP
+    if the condition is not met.
+    Source: https://stackoverflow.com/questions/17946024/deactivate-function-with-decorator
+
+    Example:
+    @if_(global_enable)
+    def do_something():
+        ...
+
+    '''
+    def noop_decorator(func):
+        return func  # pass through
+
+    def neutered_function(func):
+        def neutered(*args, **kw):
+            if warning:
+                log.warn(warning)
+            return None
+        return neutered
+
+    return noop_decorator if condition else neutered_function
+
