e8ec61d0ebe3976e3ebe7c36a66a2b9b02dca341
lbusoni <lbusoni@gmail.com>
2020-04-17 12:00:51 +0200

    add code_convention.py

---
+++ arte/code_convention.py
@@ -0,0 +1,70 @@
+"""
+A well-behaved writer class.
+
+It is important to help readability. Therefore, arte has code conventions.
+
+Rules:
+Line indentation is 4 spaces. No Tabs!
+Maximum line length is 80 characters.
+Comments should be complete sentences in English.
+Modules use short, all-lowercase names with underscores (snake_case).
+Class names use the CapWords (medial capitals) convention.
+Method names use all-lowercase with underscore (snake_case) convention.
+
+
+Eclipse Support
+The Python plugin PyDev? for Eclipse has a tool for checking source code
+on conformity with PEP8 standard. Use it!
+Set Pydev->Editor->Code Style->Code Formatter->Formatter style=Pydev.Formatter
+Check Pydev->Editor->Code Analysis->Do Code Analysis
+
+"""
+
+import os
+
+
+class NiceWriter:
+    """
+    A nice writer .
+
+    This class is an example of code styling conventions.
+    """
+
+    A_CLASS_CONSTANT = 'whatever it is, centralize constant here'
+    NUMBER_OF_SLOPES = 1600  # Not N_SLOPES. Be descriptive
+
+    def __init__(self, path):
+        self._path = path
+
+    def public_work(self, an_argument):
+        """
+        add example docstring here
+        """
+        if self._path.startswith(an_argument):
+            os.chdir(self.A_CLASS_CONSTANT)
+        else:
+            os.remove(self._path)
+
+        for i in range(0, 3):
+            self._say(i)
+
+        j = 5
+        k = 3
+        a_container = []
+        while k < 9:
+            a_container.append(j + k)
+            k = k + 1
+
+    def _say_something_private(self, x):
+        """
+        A private method.
+        A leading underscore denotes private methods.
+        """
+        print(x)
+
+
+class InafIsAnAcronym():
+    '''
+    InafIsAnAcronym is more readable than INAFIsAnAcronym
+    '''
+    pass
