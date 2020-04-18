7e39965a4bd8ac98e5a678734ed0725b9a0a533b
Alfio Puglisi <alfio.puglisi@inaf.it>
2020-04-18 11:23:10 +0200

    Fixed MultiTimeSeries bugs, added test

--- arte/time_series/multi_time_series.py
+++ arte/time_series/multi_time_series.py
@@ -6,8 +6,14 @@ from arte.time_series import TimeSeries
 
 class MultiTimeSeries(TimeSeries):
     '''
-    This class is a container for multiple TimeSeries objects
-    with potentially incompatible time sampling.
+    Join multiple TimeSeries objects with incompatible time sampling.
+    
+    After being initialized with a list of TimeSeries or objects, which can
+    have different ensemble sizes and length, it behaves as if all the data
+    was part of a single ensemble. Operations that make calculations
+    time-wise (like time_std()) work in any case. The ensemble-wise ones
+    (like ensemble_std()) only work if deltaTime is the same
+    across all series, and raise an Exception otherwise.
     '''
 
     def __init__(self, *args):
@@ -16,7 +22,10 @@ class MultiTimeSeries(TimeSeries):
 
         self._series = []
         for v in args:
-            self._series.append(v)
+            self.add_series(v)
+
+    def add_series(self, series):
+        self._series.append(series)
 
     def _get_not_indexed_data(self):
         return np.hstack([v._get_not_indexed_data()
@@ -25,14 +34,14 @@ class MultiTimeSeries(TimeSeries):
     def get_series(self, n):
         return self._series[n]
 
-    def is_homogenous(self, *args, **kwargs):
+    def is_homogeneous(self, *args, **kwargs):
 
-        dt = [x.value for x in self.delta_times(args, **kwargs)]
+        dt = [x.value for x in self.delta_times(*args, **kwargs)]
         return len(set(dt)) == 1
 
     def __impersonateDeltaTime(self, *args, **kwargs):
 
-        dt = self.delta_times(args, **kwargs)
+        dt = self.delta_times(*args, **kwargs)
         self.delta_time = dt[0]
 
     def ensemble_size(self):
@@ -54,34 +63,18 @@ class MultiTimeSeries(TimeSeries):
 
         return dt
              
-    def time_average(self, times=None, *args, **kwargs):
-        if times is None or self.is_homogenous(*args, **kwargs):
+    def ensemble_average(self, *args, **kwargs):
+        if self.is_homogeneous(*args, **kwargs):
             self.__impersonateDeltaTime(*args, **kwargs) 
             return super().time_average(*args, **kwargs)
         else:
             raise Exception('Data series cannot be combined')
         
-    def time_median(self, times=None, *args, **kwargs):
-        if times is None or self.is_homogenous(*args, **kwargs):
-            self.__impersonateDeltaTime(*args, **kwargs) 
-            return super().time_median(*args, **kwargs)
-        else:
-            raise Exception('Data series cannot be combined')
-        
-    def time_std(self, times=None, *args, **kwargs):
-        if times is None or self.is_homogenous(*args, **kwargs):
+    def ensemble_std(self, *args, **kwargs):
+        if self.is_homogeneous(*args, **kwargs):
             self.__impersonateDeltaTime(*args, **kwargs) 
             return super().time_std(*args, **kwargs)
         else:
             raise Exception('Data series cannot be combined')
         
-    def power(self, from_freq=None, to_freq=None,
-              segment_factor=None, window='boxcar', *args, **kwargs):      
-
-        if self.is_homogenous(*args, **kwargs):
-            self.__impersonateDeltaTime(*args, **kwargs) 
-            return super().power(from_freq, to_freq, segment_factor,
-                                 window, *args, **kwargs)
-        else:
-            raise Exception('Data series cannot be combined')
-
+# ___oOo___
