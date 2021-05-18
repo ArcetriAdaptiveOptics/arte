9388fbb38f9d649b17ae0bcca70951b57c63f109
Lorenzo Busoni <lbusoni@gmail.com>
2021-05-18 13:50:55 +0200

    Update phase_screen_generator_test.py

--- test/atmo/phase_screen_generator_test.py
+++ test/atmo/phase_screen_generator_test.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import unittest
 from arte.atmo.phase_screen_generator import PhaseScreenGenerator
+from arte.types.domainxy import DomainXY
+from arte.types.scalar_bidimensional_function import ScalarBidimensionalFunction
 
 
 class PhaseScreenGeneratorTest(unittest.TestCase):
@@ -24,6 +26,16 @@ class PhaseScreenGeneratorTest(unittest.TestCase):
             howMany, self._screenSizeInPixels, self._screenSizeInPixels)
         self.assertEqual(expectedShape, phaseScreens.shape)
 
+    def testA(self):
+        howMany = 2
+        wavelengthInMeters = 1e-6
+        self._psg.generateNormalizedPhaseScreens(howMany)
+        self._psg.rescaleTo(self._r0At500nm)
+        phaseScreens = self._psg.getInRadiansAt(wavelengthInMeters)
+        dxy = DomainXY.from_shape(
+            phaseScreens[0].shape,
+            pixel_size=self._screenSizeInMeters / self._screenSizeInPixels)
+        scrF = ScalarBidimensionalFunction(phaseScreens[0], domain=dxy)
 
 if __name__ == "__main__":
     unittest.main()
