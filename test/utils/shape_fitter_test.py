c9996844e83f95e95d91dca745d957cf5e3fd950
marco <marco@m4ws>
2021-06-04 12:24:55 +0200

    pep8 applied

--- test/utils/shape_fitter_test.py
+++ test/utils/shape_fitter_test.py
@@ -25,25 +25,27 @@ class PupilFitterTest(unittest.TestCase):
                                      inRadius=self.inradius)
 
     def test_docstring(self):
-        print ("In method %s" % self._testMethodName)
+        print("In method %s" % self._testMethodName)
         import arte.utils.shape_fitter as pf_module
         doctest.testmod(pf_module, raise_on_error=True)
 
     def test_ransac(self):
-        print ("In method %s" % self._testMethodName)
+        print("In method %s" % self._testMethodName)
         ff = ShapeFitter(self.testMask1.asTransmissionValue())
         ff.fit_circle_ransac()
         ff2 = ShapeFitter(self.testMask2.asTransmissionValue())
         ff2.fit_circle_ransac()
-        np.testing.assert_allclose(ff.parameters(), self.params2check, rtol=1e-2)
-        np.testing.assert_allclose(ff2.parameters(), self.params2check, rtol=1e-2)
+        np.testing.assert_allclose(
+            ff.parameters(), self.params2check, rtol=1e-2)
+        np.testing.assert_allclose(ff2.parameters(),
+                                   self.params2check, rtol=1e-2)
 
     def test_correlation_full(self):
-        print ("In method %s" % self._testMethodName)
+        print("In method %s" % self._testMethodName)
         mm = ['Nelder-Mead', 'Powell', 'CG', 'BFGS', 'L-BFGS-B',
               'TNC', 'COBYLA', 'SLSQP', 'trust-constr']
         for xx in mm:
-            print ("Tested method %s" % xx)
+            print("Tested method %s" % xx)
             ff = ShapeFitter(self.testMask1.asTransmissionValue())
             ff.fit_circle_correlation(method=xx)
             ff2 = ShapeFitter(self.testMask2.asTransmissionValue())
@@ -51,7 +53,8 @@ class PupilFitterTest(unittest.TestCase):
             p1 = ff.parameters()
             p2 = ff2.parameters()
             rtol = 0.01
-            if xx == 'CG' or xx == 'BFGS' or xx == 'L-BFGS-B' or xx == 'TNC' or xx == 'SLSQP' or xx == 'trust-constr':
+            if xx == 'CG' or xx == 'BFGS' or xx == 'L-BFGS-B' or xx == 'TNC' \
+                    or xx == 'SLSQP' or xx == 'trust-constr':
                 p1 = p1[[1, 0, 2]]
                 p2 = p2[[1, 0, 2]]
                 rtol = 3
