ef5b880d708ed9d5b20552b0116888b95c33aa8d
g-carla <giulia.carl9@gmail.com>
2023-06-20 12:27:35 +0200

    Notebook update

--- arte/photometry/sandbox.py
+++ arte/photometry/sandbox.py
@@ -20,6 +20,7 @@ from arte.photometry.transmissive_elements_catalogs import MorfeoTransmissiveEle
 from scipy import interpolate
 from arte.photometry.transmittance_calculator import internal_transmittance_calculator, \
     attenuation_coefficient_calculator, external_transmittance_calculator
+from arte.photometry.transmissive_elements import TransmissiveElement, Bandpass
 
 
 def misc():
@@ -109,6 +110,38 @@ def check_zeropoints_ESO():
         cnts = obs.countrate(area=1 * u.m ** 2, wavelengths=filt.waveset)
         print(f"{filt_name:10s} | {zp:10.3e} | {err:+7.3f}% | {cnts:10.3e} |"
               f"{filt.waveset.to(u.nm)[0]:g} {filt.waveset.to(u.nm)[-1]:g}")
+
+
+def main230614_check_expected_flux_at_LO_with_SA0():
+    '''
+    In SA0 the throughput considered for the telescope + MORFEO system (up to
+    LO WFS included) is constant: 0.327 in H band that is defined from 1485 to
+    1815 nm.
+    '''
+    filt_LO_name = Filters.BESSEL_H
+    
+    zenith_angle = 30 * u.deg
+    airmass = 1 / np.cos(zenith_angle.to(u.rad))
+    f_vega = get_normalized_star_spectrum(spectral_type='vega', magnitude=0,
+                                          filter_name=filt_LO_name)
+    
+    sky = EsoSkyCalc(airmass=airmass, incl_moon='N')
+    sky_se = SpectralElement(Empirical1D, points=sky.lam, lookup_table=sky.trans)
+    
+    lo_ch_constant_transmittance = TransmissiveElement(
+        transmittance=Bandpass.top_hat(
+            peak_wl=1650 * u.nm, delta_wl=165 * u.nm, high_ampl=0.327, low_ampl=0.),
+        reflectance=Bandpass.zero())
+    
+    filt = sky_se * lo_ch_constant_transmittance.transmittance
+    
+    obs = Observation(spec=f_vega, band=filt, force='taper', binset=f_vega.waveset)
+    
+    area_subap = 1 * u.m ** 2
+    counts = obs.countrate(area=area_subap)
+    exp_time = 1 * u.s
+    print('Expected flux at LO WFS in ph/s/m2 (x1e8): %s'
+          % ((counts * exp_time).decompose() / 1e8))
 
 
 def main230420_derive_VISIR_dichroic_transmittance_from_LZH():
