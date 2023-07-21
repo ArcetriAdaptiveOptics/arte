907e6f9ad2379141d3d7ae155705db2bdc6ae1f5
g-carla <giulia.carl9@gmail.com>
2023-07-21 16:11:14 +0200

    Add main to compare LGS dichroic coating curves.

---
+++ arte/photometry/mains/main230721_lgs_dichroic_coating.py
@@ -0,0 +1,30 @@
+import astropy.units as u
+import matplotlib.pyplot as plt
+from arte.photometry.transmissive_elements_catalogs import CoatingsTransmissiveElementsCatalog
+
+
+def main230721_comparison_of_LMA_coating_curves():
+    '''
+    '''
+    exp_ave = CoatingsTransmissiveElementsCatalog.lma_exp_ave_001()
+    exp_min = CoatingsTransmissiveElementsCatalog.lma_exp_min_001()
+    env_min = CoatingsTransmissiveElementsCatalog.lma_env_min_001()
+    
+    wv = exp_ave.waveset
+    plt.figure()
+    plt.plot(wv.to(u.um), env_min.transmittance(wv), label='env min')
+    plt.plot(wv.to(u.um), exp_min.transmittance(wv), label='exp min')
+    plt.plot(wv.to(u.um), exp_ave.transmittance(wv), label='exp ave')
+    plt.grid()
+    plt.legend()
+    plt.xlabel('Wavelength [µm]')
+    plt.ylabel('Transmittance')
+    
+    plt.figure()
+    plt.plot(wv.to(u.um), env_min.reflectance(wv), label='env min')
+    plt.plot(wv.to(u.um), exp_min.reflectance(wv), label='exp min')
+    plt.plot(wv.to(u.um), exp_ave.reflectance(wv), label='exp ave')
+    plt.grid()
+    plt.legend()
+    plt.xlabel('Wavelength [µm]')
+    plt.ylabel('Reflectance')
