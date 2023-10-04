7eb6d21e45dd8d963e6f4fa3631b07a0d25f5c38
g-carla <giulia.carl9@gmail.com>
2023-10-04 17:23:18 +0200

    Add main on thermal bkg

--- arte/photometry/mains/main230921_thermal_flux_on_wfs.py
+++ arte/photometry/mains/main230921_thermal_flux_on_wfs.py
@@ -2,13 +2,17 @@ import numpy as np
 import astropy.units as u
 import matplotlib.pyplot as plt
 from arte.photometry.morfeo_transmissive_systems import MorfeoLowOrderChannelTransmissiveSystem_003, \
-    MorfeoReferenceChannelTransmissiveSystem_004
+    MorfeoReferenceChannelTransmissiveSystem_004, MorfeoMainPathOptics_003, \
+    EltTransmissiveSystem
 from arte.photometry.transmissive_elements_catalogs import DetectorsCatalog, \
     MorfeoTransmissiveElementsCatalog
-from morfeo.utils.constants import MORFEO
+from morfeo.utils.constants import MORFEO, ELT
 from synphot.spectrum import SourceSpectrum
-from synphot.models import BlackBody1D
+from synphot.models import BlackBody1D, Empirical1D
 from synphot import units
+from arte.photometry.eso_sky_calc import EsoSkyCalc
+from arte.photometry.filters import Filters
+from synphot.observation import Observation
 
 
 def thermal_flux_on_LO_WFS():
@@ -87,3 +91,73 @@ def thermal_flux_on_R_WFS():
     
     print(np.trapz(th_flux, waveset.to(u.Angstrom)))
     return th_flux, waveset
+
+
+def MPO_thermal_flux():
+    '''
+    Thermal flux on the MPO focal plane due to the emissivity of the elements
+    in the MPO optical path (from MORFEO entrance focal plane 
+    - CPM approximately - to MICADO entrance focal plane).
+    '''
+    T = 278.15 * u.K
+    bb_spectrum = SourceSpectrum(BlackBody1D, temperature=T)
+    mpo_transmissive_system = MorfeoMainPathOptics_003()
+    sterad_to_arcsec2 = 206265 ** 2
+    filt = Filters.get(Filters.ESO_ETC_K)
+    obs = Observation(bb_spectrum * mpo_transmissive_system.emissivity,
+                      filt)
+    flux = obs.countrate(
+            area=ELT.photometric_area, wavelengths=filt.waveset
+            ) / sterad_to_arcsec2
+    return flux
+
+
+def ELT_thermal_flux():
+    '''
+    Thermal flux on the ELT focal plane due to the emissivity of the elements
+    in the ELT optical path.
+    '''
+    T = 278.15 * u.K
+    bb_spectrum = SourceSpectrum(BlackBody1D, temperature=T)
+    elt_transmissive_system = EltTransmissiveSystem()
+    sterad_to_arcsec2 = 206265 ** 2
+    filt = Filters.get(Filters.ESO_ETC_K)
+    obs = Observation(bb_spectrum * elt_transmissive_system.emissivity,
+                      filt)
+    flux = obs.countrate(
+            area=ELT.photometric_area, wavelengths=filt.waveset
+            ) / sterad_to_arcsec2
+    return flux
+
+
+def sky_flux_at_ELT_exit(incl_moon='N', spec_mag=True):
+    elt_ts = EltTransmissiveSystem()
+    filt = Filters.get(Filters.ESO_ETC_K)
+    z = 30 * u.deg
+    airmass = 1 / np.cos(z)
+    sky = EsoSkyCalc(airmass=airmass, incl_moon=incl_moon)
+    sky_flux_in_photlam_unit = sky.flux.to(
+        u.ph / u.arcsec ** 2 / u.cm ** 2 / u.s / u.Angstrom)
+    sky_sp = SourceSpectrum(
+        Empirical1D, points=sky.lam.to(u.Angstrom),
+        lookup_table=sky_flux_in_photlam_unit.value * units.PHOTLAM)
+    obs = Observation(sky_sp * elt_ts.transmittance, filt)
+    flux = obs.countrate(
+            area=ELT.photometric_area, wavelengths=filt.waveset
+            )
+    if spec_mag is True:
+        mag_from_eso_sky_calc = 15.09
+        mag_spec = 13.5
+        flux *= 10 ** ((mag_from_eso_sky_calc - mag_spec) / 2.5)
+    return flux
+
+
+def main230922_check_bkg_spec(incl_moon='N', spec_mag=True):
+    th_fl_mpo = MPO_thermal_flux()
+    th_fl_elt = ELT_thermal_flux()
+    sky_flux_elt = sky_flux_at_ELT_exit(incl_moon, spec_mag)
+    
+    print(th_fl_mpo)
+    print(th_fl_elt)
+    print(sky_flux_elt)
+    print(th_fl_mpo / (th_fl_elt + sky_flux_elt))
