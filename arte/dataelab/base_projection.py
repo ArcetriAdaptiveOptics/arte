3bfc4934f4e8aea77f86aabf798d4e518252c8d5
Alfio Puglisi <alfio.puglisi@inaf.it>
2024-04-26 14:29:22 +0200

    BaseProjection now uses new @cache_on_disk decorator

--- arte/dataelab/base_projection.py
+++ arte/dataelab/base_projection.py
@@ -1,7 +1,6 @@
-import numpy as np
 from arte.dataelab.base_timeseries import BaseTimeSeries
 from arte.utils.not_available import NotAvailable
-from arte.utils.cache_on_disk import cache_on_disk
+from arte.dataelab.cache_on_disk import cache_on_disk
 from arte.dataelab.data_loader import OnTheFlyLoader
 
 
@@ -15,7 +14,7 @@ class BaseProjection(BaseTimeSeries):
                              loader_or_data=OnTheFlyLoader(self.project),
                              mapper2d=mapper2d,
                              astropy_unit=astropy_unit)
-        except Exception as e:
+        except Exception:
             NotAvailable.transformInNotAvailable(self)
             return
         self._source_timeseries = source_timeseries
@@ -23,10 +22,9 @@ class BaseProjection(BaseTimeSeries):
 
     @cache_on_disk
     def project(self):
+        '''Perform projection'''
         data = self._source_timeseries.get_data()
         proj = self._projection_matrix.get_data()
-        print(data.shape, proj.shape)
-        print(data, proj)
         if isinstance(data, NotAvailable) or isinstance(proj, NotAvailable):
             data = NotAvailable()
         else:
