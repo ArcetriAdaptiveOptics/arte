d934660c31d428119e140ad56d6f5af7fc1d41dc
Alfio Puglisi <alfio.puglisi@inaf.it>
2024-04-27 13:29:50 +0200

    New signal_unit for slopes

--- test/dataelab/base_residual_modes_test.py
+++ test/dataelab/base_residual_modes_test.py
@@ -5,8 +5,9 @@ import unittest
 import numpy as np
 import astropy.units as u
 from arte.dataelab.base_data import BaseData
-from arte.dataelab.base_slopes import BaseSlopes
+from arte.dataelab.base_slopes import BaseSlopes, signal_unit
 from arte.dataelab.base_residual_modes import BaseResidualModes
+
 
 class BaseResidualModesTest(unittest.TestCase):
 
@@ -25,8 +26,25 @@ class BaseResidualModesTest(unittest.TestCase):
                                       self._expected_data)
 
     def test_unit(self):
+        '''Test that using assignment from unitless data works'''
         assert self._resmodes.get_data().unit == u.m
 
     def test_indexer(self):
+        '''Test that the indexer for single modes works'''
         np.testing.assert_array_equal(self._resmodes.get_data(mode=1),
                                       self._expected_data[:,1])
+
+    def test_unit_combination(self):
+        '''Test that ResidualModes multiplies input units correctly'''
+        self._data = np.arange(6).reshape((3,2)) * signal_unit
+        self._proj = np.array([[1,2], [3,4]]) * (u.m / signal_unit)
+        self._slopes = BaseSlopes(1*u.s, self._data)
+        self._modalrec = BaseData(self._proj)
+        self._resmodes = BaseResidualModes(self._slopes,
+                                           self._modalrec,
+                                           astropy_unit = None)
+        self._expected_data = self._data.value @ self._proj.value * u.m
+        np.testing.assert_array_equal(self._resmodes.get_data(mode=1),
+                                      self._expected_data[:,1])
+
+# __oOo__
