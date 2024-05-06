f43cc5273a80a1d95d4e296a0ea5f7cf49c64cb2
Alfio Puglisi <alfio.puglisi@inaf.it>
2024-05-06 17:36:01 +0200

    Removed TimeSeriesWithInterpolation

--- test/time_series/time_series_test.py
+++ test/time_series/time_series_test.py
@@ -4,7 +4,7 @@ import math
 import numpy as np
 import astropy.units as u
 
-from arte.time_series.time_series import TimeSeries, TimeSeriesWithInterpolation
+from arte.time_series.time_series import TimeSeries
 from arte.time_series.indexer import Indexer, ModeIndexer, RowColIndexer
 from arte.utils.not_available import NotAvailable
 
@@ -55,8 +55,8 @@ class TimeSeries1D(TimeSeries):
     def _get_not_indexed_data(self):
         return np.arange(20).reshape(5,4)
     
-    def get_index_of(*args, **kwargs):
-        return args[0]
+    def get_index_of(self, *args, **kwargs):
+        return args[0]   # Modified inside tests
 
 
 class TimeSeries2D(TimeSeries):
@@ -64,7 +64,7 @@ class TimeSeries2D(TimeSeries):
     def _get_not_indexed_data(self):
         return np.arange(2*3*4).reshape(2,3,4)
     
-    def get_index_of(*args, **kwargs):
+    def get_index_of(self, *args, **kwargs):
         pass  # Modified inside tests    
     
 
@@ -201,140 +201,6 @@ class TimeSeriesTest(unittest.TestCase):
         assert test.shape == (2, 2, 3)    # [Time, rows, cols]
 
 
-class AnIncompleteTimeSeries1(TimeSeriesWithInterpolation):
-    '''A time series with a single gap a'''
-
-    test_data = np.arange(16).reshape((4, 4))   #Original data
-    test_counter = np.array([0,1,3,4], dtype=np.uint32)  # Original counter
-
-    ref_counter = np.array([0,1,2,3,4], dtype=np.uint32)
-    ref_data_shape = (5,4)
-    ref_data = [np.array([6,7,8,9], dtype=np.float32)]
-    ref_slice = [np.s_[2,:]]  # Where to find the interpolated data
-    
-    def get_interpolation(self, new_data):
-        return [new_data[_slice] for _slice in self.ref_slice]
-
-    def get_original(self, new_data):
-        mask = np.ones(new_data.shape,dtype=bool)
-        for _slice in self.ref_slice:
-            mask[_slice]= False
-        return new_data[mask].reshape(self.test_data.shape)
-
-    def _get_not_indexed_data(self):
-        data = self.test_data
-        return self.interpolate_missing_data(data)
-    
-    def _get_counter(self):
-        return self.test_counter
-
-    def get_index_of(self, *args, **kwargs):
-        return Indexer.myrange(*args, maxrange=4)
-    
-
-class AnIncompleteTimeSeries2(AnIncompleteTimeSeries1):
-    '''A time series with a single gap and a counter going up by 2'''
-    
-    test_data = np.arange(16).reshape((4, 4))
-    test_counter = np.array([0,2,6,8], dtype=np.uint32)
-    ref_counter = np.array([0,2,4,6,8], dtype=np.uint32)
-    ref_data_shape = (5,4)
-    ref_data = [np.array([6,7,8,9], dtype=np.float32)]
-    ref_slice = [np.s_[2,:]]
-
-class AnIncompleteTimeSeries3(AnIncompleteTimeSeries1):
-    '''A time series with no gaps'''
-    
-    test_data = np.arange(16).reshape((4, 4))
-    test_counter = np.array([0,1,2,3], dtype=np.uint32)
-    ref_counter = np.array([0,1,2,3], dtype=np.uint32)
-    ref_data_shape = (4,4)
-    ref_data = []
-    ref_slice = []
-
-class AnIncompleteTimeSeries4(AnIncompleteTimeSeries1):
-    '''A time series with three separate gaps'''
-
-    test_data = np.arange(40).reshape((10, 4))
-    test_counter = np.array([0,1,2,6,7,8,9,11,13,14], dtype=np.uint32)
-    ref_counter = np.arange(15, dtype=np.uint32)
-    ref_data_shape = (15,4)
-    ref_data = [np.array([[9,10,11,12],
-                                        [10,11,12,13],
-                                        [11,12,13,14]], dtype=np.float32),
-                              np.array([26,27,28,29], dtype=np.float32),
-                              np.array([30,31,32,33], dtype=np.float32)]
-                                       
-    ref_slice = [np.s_[3:6,:], np.s_[10,:], np.s_[12,:]]
-
-
-class AnIncompleteTimeSeries5(AnIncompleteTimeSeries4):
-    '''A time series with three separate gaps, with a strange counter'''
-
-    test_counter = AnIncompleteTimeSeries4.test_counter*0.1 - 10
-    ref_counter = AnIncompleteTimeSeries4.ref_counter*0.1 - 10
-
-class AnIncompleteTimeSeries6(AnIncompleteTimeSeries4):
-    '''A time series with an incorrect frame counter length'''
-
-    # Should have been 10
-    test_counter = np.arange(8)
-
-class CounterIsNotAvailable(AnIncompleteTimeSeries4):
-    
-    test_counter = NotAvailable()
-
-class TimeSeriesWithInterpolationTest(unittest.TestCase):
-
-    def _test_interpolation(self, classname):
-
-        series = classname(sampling_interval=1)
-        
-        origin_counter = series.get_original_counter()
-        interp_counter = series.get_counter()
-        data = series.get_data()
-        
-        np.testing.assert_array_equal(origin_counter, series.test_counter)
-        assert np.allclose(interp_counter, series.ref_counter)
-        for interpolation, ref in zip(series.get_interpolation(data),
-                                      series.ref_data):
-            assert np.allclose(interpolation, ref)
-            
-        np.testing.assert_array_equal(series.get_original(data),
-                                      series.test_data)
-
-        assert data.shape == series.ref_data_shape
-
-    @unittest.skip("TimeSeriesWithInterpolation must be updated")
-    def test_interpolation_with_step1(self):
-        self._test_interpolation(AnIncompleteTimeSeries1)
-
-    @unittest.skip("TimeSeriesWithInterpolation must be updated")
-    def test_interpolation_with_step2(self):
-        self._test_interpolation(AnIncompleteTimeSeries2)
-
-    @unittest.skip("TimeSeriesWithInterpolation must be updated")
-    def test_interpolation_with_step3(self):
-        self._test_interpolation(AnIncompleteTimeSeries3)
-
-    @unittest.skip("TimeSeriesWithInterpolation must be updated")
-    def test_interpolation_with_step4(self):
-        self._test_interpolation(AnIncompleteTimeSeries4)
-
-    @unittest.skip("TimeSeriesWithInterpolation must be updated")
-    def test_interpolation_with_step5(self):
-        self._test_interpolation(AnIncompleteTimeSeries5)
-
-    @unittest.skip("TimeSeriesWithInterpolation must be updated")
-    def test_interpolation_with_step6(self):
-        with self.assertRaises(ValueError):
-            self._test_interpolation(AnIncompleteTimeSeries6)
-    
-    @unittest.skip("TimeSeriesWithInterpolation must be updated")
-    def test_counter_not_available(self):
-        
-        series = CounterIsNotAvailable()
-        assert isinstance(series.get_data(), NotAvailable)
 
 if __name__ == "__main__":
     unittest.main()
