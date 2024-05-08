3f46ce32fe2be34b08a9312038a4b1ac9199db05
Alfio Puglisi <alfio.puglisi@gmail.com>
2024-05-08 14:18:36 +0200

    Fixed bug in BaseProjection

--- arte/dataelab/base_projection.py
+++ arte/dataelab/base_projection.py
@@ -3,7 +3,6 @@ from arte.utils.not_available import NotAvailable
 from arte.dataelab.cache_on_disk import cache_on_disk
 from arte.dataelab.data_loader import OnTheFlyLoader
 
-
 class BaseProjection(BaseTimeSeries):
 
     def __init__(self, source_timeseries, projection_matrix, astropy_unit=None, data_label=None):
@@ -11,12 +10,12 @@ class BaseProjection(BaseTimeSeries):
             assert not isinstance(source_timeseries, NotAvailable)
             assert not isinstance(projection_matrix, NotAvailable)
             super().__init__(loader_or_data=OnTheFlyLoader(self.project),
-                             time_vector=OnTheFlyLoader(source_timeseries.time_vector),
+                             time_vector=OnTheFlyLoader(source_timeseries.get_time_vector),
                              astropy_unit=astropy_unit,
                              data_label=data_label)
             self._unit_handler.set_force(True)
 
-        except Exception:
+        except AssertionError as e:
             NotAvailable.transformInNotAvailable(self)
             return
         self._source_timeseries = source_timeseries
@@ -27,6 +26,7 @@ class BaseProjection(BaseTimeSeries):
         '''Perform projection'''
         data = self._source_timeseries.get_data()
         proj = self._projection_matrix.get_data()
+        print(data, proj)
         if isinstance(data, NotAvailable) or isinstance(proj, NotAvailable):
             data = NotAvailable()
         else:
