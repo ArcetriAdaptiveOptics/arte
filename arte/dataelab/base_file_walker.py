5221486d40d17c3bcd7b5822644d3a8aa3da6cd1
Alfio Puglisi <alfio.puglisi@gmail.com>
2024-05-08 17:13:32 +0200

    docstrings, typos

--- arte/dataelab/base_file_walker.py
+++ arte/dataelab/base_file_walker.py
@@ -4,7 +4,7 @@ class AbstractFileNameWalker(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
     def snapshot_dir(self, tag):
-        '''Return the director where a single snapshot is stored'''
+        '''Return the directory with a specific snapshot's contents'''
 
     def find_tag_between_dates(self, tag_start, tag_stop):
         '''Return the list of tags between the two extremes'''
--- test/dataelab/base_file_walker_test.py
+++ test/dataelab/base_file_walker_test.py
@@ -4,6 +4,7 @@ from arte.dataelab.base_file_walker import AbstractFileNameWalker
 class BaseFileWalkerTest(unittest.TestCase):
 
     def test_abstract(self):
+        '''Test that a derived class must implement abstract methods'''
         class TestFileWalker(AbstractFileNameWalker):
             pass
         with self.assertRaises(TypeError):
